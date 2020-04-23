5a1714f21fe24ed79a9dbf6299522014b81825b2
Micah Evans <micah.s.evans@gmail.com>
2020-04-23 18:21:22 -0400

    Changes made to employeeList.json and code added to EMployee.js to map the character's name

--- employee-directory/src/components/Employee.js
+++ employee-directory/src/components/Employee.js
@@ -2,7 +2,19 @@ import React from "react";
 
 function Employee(props) {
     return (
-       <div>
-       </div> 
+        <div>
+            {props.employeeList.length ? (
+                <ul className="list">
+                    {props.employeeList.map(result => (
+                        <li className="itemList" key={result.id}>
+                            <div id="name">{result.name} ({result.type})</div>
+                            <div id="email">Contact: {result.email} </div>
+                        </li>
+                    ))}
+                </ul>
+            ) : (<p>Sorry, this hero does not exist.</p>)}
+       </div>
     )
 }
+
+export default Employee;
